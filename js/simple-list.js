7cc98900457f2969e9f8f9ad08d7ad3f32c24f4a
Gabriel BuenoAzevedo <bielalgumacoisa@gmail.com>
2020-06-22 23:26:00 -0300

    [UPT] Changed file structure and implemented queue for clicks

---
+++ js/simple-list.js
@@ -0,0 +1,53 @@
+const button = document.getElementById('button');
+const nodes = document.getElementsByClassName('node');
+const node_container = document.getElementById('node-container');
+const max_animation_time = 1500;
+let creation_queue = [];
+
+//Create a new node for the list.
+createNewNode = () => {
+  const animation_time = startAnimation();
+  window.setTimeout( () => {
+    const new_node = document.createElement('div');
+    new_node.classList.add('node');
+    node_container.appendChild(new_node);
+  }, animation_time);
+}
+
+//Starts last node's arrow animation. Returns time to execute the animation.
+startAnimation = () => {
+  if (nodes.length > 0){
+    const last_node = nodes[nodes.length-1];
+    last_node.classList.add('node-arrow');
+    return max_animation_time;
+  }
+  return 0;
+}
+
+//Adds createNode to creation queue
+addNodeToCreationQueue = () => {
+  let animation_time = 0;
+  console.log(creation_queue);
+  if (nodes.length > 0 ){
+    animation_time = max_animation_time;
+  }
+  creation_queue.push(animation_time);
+  if (creation_queue.length === 1) 
+    runCreationQueue();
+}
+
+//Starts running creation queue
+runCreationQueue = () => {
+  if (creation_queue.length > 0){
+    const animation_time = creation_queue[0];
+    console.log(animation_time)
+    window.setTimeout( () => {
+      createNewNode();
+      creation_queue.shift();
+      runCreationQueue();
+    }, animation_time)
+  }
+}
+
+button.onclick = addNodeToCreationQueue;
+
